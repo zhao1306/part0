49fca6cb56c442ad62756703462dd9aab825ddd0
zhao1306 <zhao1306@purdue.edu>
2026-04-14 17:53:06 -0400

    1.6 complete

--- part1_exercises/unicafe/src/App.jsx
+++ part1_exercises/unicafe/src/App.jsx
@@ -1,16 +1,57 @@
-import { useState } from 'react'
+import { useState } from "react";
+
+const Button = ({ onClick, name }) => <button onClick={onClick}>{name}</button>;
+
+const Feedback = ({ addGood, addNeutral, addBad }) => {
+  console.log(addGood);
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={addGood} name="good" />
+      <Button onClick={addNeutral} name="neutral" />
+      <Button onClick={addBad} name="bad" />
+    </div>
+  );
+};
+
+const Stats = ({ good, neutral, bad }) => {
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <p>{good}</p>
+      <p>{neutral}</p>
+      <p>{bad}</p>
+    </div>
+  );
+};
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const addGood = () => {
+    const newGood = good + 1;
+    setGood(newGood);
+  };
+
+  const addNeutral = () => {
+    const newNeutral = neutral + 1;
+    setNeutral(newNeutral);
+  };
+
+  const addBad = () => {
+    const newBad = bad + 1;
+    setBad(newBad);
+  };
 
   return (
     <div>
-      code here
+      <Feedback addGood={addGood} addNeutral={addNeutral} addBad={addBad} />
+      <Stats good={good} neutral={neutral} bad={bad} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
