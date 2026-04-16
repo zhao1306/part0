852393bf2074cd6d0e003e91febf49e132105018
zhao1306 <zhao1306@purdue.edu>
2026-04-16 15:54:31 -0400

    Elightenment achieved, 2.5 complete

--- part2_exercises/course_info_cont/src/App.jsx
+++ part2_exercises/course_info_cont/src/App.jsx
@@ -1,42 +1,4 @@
-const Header = (props) => <h1>{props.course}</h1>;
-
-const Content = (props) => (
-  <div>
-    {props.parts.map((part) => (
-      <Part key={part.id} part={part} />
-    ))}
-  </div>
-);
-
-const Part = (props) => (
-  <p>
-    {props.part.name} {props.part.exercises}
-  </p>
-);
-
-const Total = (props) => {
-  const parts = props.parts;
-  const total = parts
-    .map((part) => part.exercises)
-    .reduce((prev, next) => prev + next);
-  return (
-    <div>
-      <p>Number of exercises {total}</p>
-    </div>
-  );
-};
-
-const Course = (props) => {
-  console.log("incourse", props);
-  const { id, name, parts } = props.course;
-  return (
-    <div>
-      <Header course={name} />
-      <Content parts={parts} />
-      <Total parts={parts} />
-    </div>
-  );
-};
+import Course from "./Course.jsx";
 
 const App = () => {
   const courses = [
