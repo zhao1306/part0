d01619425a1a0e3702e15ce258ade812144a229f
zhao1306 <zhao1306@purdue.edu>
2026-04-16 17:15:40 -0400

    2. 67

---
+++ part2_exercises/phonebook/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleForm = (event) => {
+    event.preventDefault();
+    console.log(newName, " is the new name");
+    const newPerson = {
+      name: newName,
+    };
+    const names = persons.map((person) => person.name);
+    if (names.includes(newName)) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      setPersons(persons.concat(newPerson));
+      setNewName("");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleForm}>
+        <div>
+          name:{" "}
+          <input onChange={(e) => setNewName(e.target.value)} value={newName} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
+      <div>debug: {newName}</div>
+    </div>
+  );
+};
+
+export default App;
