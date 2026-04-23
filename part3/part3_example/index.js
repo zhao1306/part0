9b2d34b32b1602f338dd17c41c0d0bbaaf0d13f6
zhao1306 <zhao1306@purdue.edu>
2026-04-23 18:50:20 -0400

    exercise 3.1

--- part3/part3_example/index.js
+++ part3/part3_example/index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+const generateId = () => {
+  const maxId =
+    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
+  return String(maxId + 1);
+};
 
 let notes = [
   {
@@ -47,8 +52,22 @@ app.delete("/api/notes/:id", (request, response) => {
 });
 
 app.post("/api/notes", (request, response) => {
-  const note = request.body;
-  console.log(note);
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
   response.json(note);
 });
 
---
+++ part3/part3_exercise/index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const generateId = () => {
+  const maxId =
+    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
+  return String(maxId + 1);
+};
+
+let data = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/persons", (request, response) => {
+  console.log(data);
+  response.json(data);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
