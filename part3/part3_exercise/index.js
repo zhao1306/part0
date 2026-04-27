e7850718b70aa8eea5431673ada5dca6bca738d1
zhao1306 <zhao1306@purdue.edu>
2026-04-27 18:28:02 -0400

    3.9-3.11 completely deployed

--- part3/part3_exercise/index.js
+++ part3/part3_exercise/index.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.static("dist"));
+//sigma
 
 morgan.token("body", (req) => {
   if (req.method === "POST" || req.method === "PUT") {
